34f78e284bcee49b03d8dd89c70165c1f9641642
Sammith1 <hsammith@gmail.com>
2022-05-28 21:04:29 +0530

    first commit

---
+++ reactjs_ballon_assignment/src/Comopnents/Home.jsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import "./Home.css";
+export const Home = () => {
+  const [num, setNum] = useState();
+
+  const firstcircle_hiding = () => {
+    document.getElementsByClassName("first_circle")[0].style.display = "none";
+    document.getElementsByClassName("empty_div_first_circle")[0].style.display =
+      "block";
+  };
+
+  const secondcircle_hiding = () => {
+    document.getElementsByClassName("second_circle")[0].style.display = "none";
+    document.getElementsByClassName(
+      "empty_div_second_circle"
+    )[0].style.display = "block";
+  };
+  const thirdcircle_hiding = () => {
+    document.getElementsByClassName("third_circle")[0].style.display = "none";
+    document.getElementsByClassName("empty_div_third_circle")[0].style.display =
+      "block";
+  };
+  const fourthcircle_hiding = () => {
+    document.getElementsByClassName("fourth_circle")[0].style.display = "none";
+    document.getElementsByClassName(
+      "empty_div_fourth_circle"
+    )[0].style.display = "block";
+  };
+  const fifthcircle_hiding = () => {
+    document.getElementsByClassName("fifth_circle")[0].style.display = "none";
+    document.getElementsByClassName("empty_div_fifth_circle")[0].style.display =
+      "block";
+  };
+
+  const remove_first_circle = () => {
+    document.getElementsByClassName("first_circle")[0].style.display = "block";
+    document.getElementsByClassName("empty_div_first_circle")[0].style.display =
+      "none";
+  };
+  const remove_second_circle = () => {
+    document.getElementsByClassName("second_circle")[0].style.display = "block";
+    document.getElementsByClassName(
+      "empty_div_second_circle"
+    )[0].style.display = "none";
+  };
+  const remove_third_circle = () => {
+    document.getElementsByClassName("third_circle")[0].style.display = "block";
+    document.getElementsByClassName("empty_div_third_circle")[0].style.display =
+      "none";
+  };
+  const remove_fourth_circle = () => {
+    document.getElementsByClassName("fourth_circle")[0].style.display = "block";
+    document.getElementsByClassName(
+      "empty_div_fourth_circle"
+    )[0].style.display = "none";
+  };
+  const remove_fifth_circle = () => {
+    document.getElementsByClassName("fifth_circle")[0].style.display = "block";
+    document.getElementsByClassName("empty_div_fifth_circle")[0].style.display =
+      "none";
+  };
+  const passing = (num) => {
+    if (num == 1) {
+      firstcircle_hiding();
+    } else if (num == 2) {
+      secondcircle_hiding();
+    } else if (num == 3) {
+      thirdcircle_hiding();
+    } else if (num == 4) {
+      fourthcircle_hiding();
+    } else if (num == 5) {
+      fifthcircle_hiding();
+    } else {
+      alert("Enter with 5 Number");
+    }
+  };
+  return (
+    <>
+      <div className="main_div">
+        <h5>empty</h5>
+        <div className="empty_div">
+          <div
+            onClick={() => {
+              remove_first_circle();
+            }}
+            className="empty_div_first_circle"
+          ></div>
+          <div
+            onClick={() => {
+              remove_second_circle();
+            }}
+            className="empty_div_second_circle"
+          ></div>
+          <div
+            onClick={() => {
+              remove_third_circle();
+            }}
+            className="empty_div_third_circle"
+          ></div>
+          <div
+            onClick={() => {
+              remove_fourth_circle();
+            }}
+            className="empty_div_fourth_circle"
+          ></div>
+          <div
+            onClick={() => {
+              remove_fifth_circle();
+            }}
+            className="empty_div_fifth_circle"
+          ></div>
+        </div>
+        <h5>circle</h5>
+        <div className="circle_div">
+          <div className="first_circle"></div>
+          <div className="second_circle"></div>
+          <div className="third_circle"></div>
+          <div className="fourth_circle"></div>
+          <div className="fifth_circle"></div>
+        </div>
+        <div>
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter Number"
+          />
+          <button
+            onClick={() => {
+              passing(num);
+            }}
+          >
+            Shoot
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
